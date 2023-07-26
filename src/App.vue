65fabfaccd5cbbdb1b390ffd5491ea18f0016a24
Bharath-designer <bharathv.developer@gmail.com>
2023-07-26 18:34:46 +0530

    export-added

--- src/App.vue
+++ src/App.vue
@@ -5,55 +5,59 @@
       @exportFile="exportFile"
       @newFile="newFile"
     />
-    <table>
-      <tbody>
-        <tr>
-          <td class="head"></td>
-          <td
-            v-if="data[0]"
-            @click="handleColHeadClick"
-            :class="`head col_head col_head${ind}`"
-            :data-key="ind"
-            :key="ind"
-            v-for="(el, ind) in Math.max(data[0].length, minColumn)"
+    <div ref="dropListener" class="drop-listener"></div>
+    <div class="table-wrapper">
+      <table>
+        <tbody>
+          <tr>
+            <td class="head"></td>
+            <td
+              v-if="data[0]"
+              @click="handleColHeadClick"
+              :class="`head col_head col_head${ind}`"
+              :data-key="ind"
+              :key="ind"
+              v-for="(el, ind) in Math.max(data[0].length, minColumn)"
+            >
+              <span>c{{ ind + 1 }}</span>
+            </td>
+          </tr>
+          <tr
+            :key="`row${rowInd}`"
+            :class="`row row${rowInd}`"
+            v-for="(cols, rowInd) in data"
           >
-            <span>c{{ ind + 1 }}</span>
-          </td>
-        </tr>
-        <tr
-          :key="`row${rowInd}`"
-          :class="`row row${rowInd}`"
-          v-for="(cols, rowInd) in data"
-        >
-          <td
-            :data-key="rowInd"
-            @click="handleRowHeadClick"
-            :key="rowInd"
-            :class="`head row_head row_head${rowInd}`"
-          >
-            <span>r{{ rowInd + 1 }}</span>
-          </td>
-          <td
-            tabindex="1"
-            :data-key="`${rowInd}-${colInd}`"
-            :key="`${rowInd}-${colInd}`"
-            :class="`col col${colInd}`"
-            v-for="(cell, colInd) in Math.max(cols.length, minColumn)"
-            @click="handleCellClick"
-            @dblclick="handleDblCellClick"
-          >
-            <input
-              :spellcheck="false"
+            <td
+              :data-key="rowInd"
+              @click="handleRowHeadClick"
+              :key="rowInd"
+              :class="`head row_head row_head${rowInd}`"
+            >
+              <span>r{{ rowInd + 1 }}</span>
+            </td>
+            <td
               :data-key="`${rowInd}-${colInd}`"
-              :value="cols[colInd]"
-              @input="inputChangeHandler"
-              readonly
-              type="text"
-            />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              :key="`${rowInd}-${colInd}`"
+              :class="`col col${colInd}`"
+              v-for="(cell, colInd) in Math.max(cols.length, minColumn)"
+              @click="handleCellClick"
+              @dblclick="handleDblCellClick"
+              tabindex="1"
+              @focusin="$event.target.click()"
+            >
+              <input
+                :spellcheck="false"
+                :data-key="`${rowInd}-${colInd}`"
+                :value="cols[colInd]"
+                @input="inputChangeHandler"
+                readonly
+                type="text"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -70,12 +74,38 @@ export default {
       data: [],
       minColumn: 15,
       minRow: 15,
-      sheetName: ""
+      sheetName: "",
     };
   },
 
   mounted() {
     fetch("./sheet.csv").then((res) => this.handleFile(undefined, res));
+
+    let dropListener = this.$refs["dropListener"];
+
+    document.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+      dropListener.classList.add("active");
+    });
+    dropListener.addEventListener("dragleave", (e) => {
+      dropListener.classList.remove("active");
+    });
+    document.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    document.addEventListener("drop", (e) => {
+      e.preventDefault();
+      dropListener.classList.remove("active");
+
+      let files = e.dataTransfer.files;
+
+      if (files.length != 1) {
+        alert("Please upload one file");
+        return;
+      }
+
+      this.handleFile(undefined, files[0]);
+    });
   },
 
   methods: {
@@ -183,7 +213,16 @@ export default {
     exportFile() {
       let csvContent =
         "data:text/csv;charset=utf-8," +
-        this.data.map((e) => e.join(",")).join("\n");
+        this.data
+          .map((e) =>
+            e.map((col) => {
+              if (col.includes(",")) {
+                return '"' + col + '"';
+              }
+              return col;
+            })
+          )
+          .join("\r\n");
       let encodedUri = encodeURI(csvContent);
       let link = document.createElement("a");
       link.setAttribute("href", encodedUri);
@@ -192,8 +231,11 @@ export default {
     },
     newFile() {
       this.data = [[]];
-      this.minColumn = parseInt(prompt("col"));
-      this.minRow = parseInt(prompt("row"));
+
+      let r = parseInt(prompt("Number of Row"));
+      let c = parseInt(prompt("Number of Column"));
+      this.minRow = isNaN(r) ? 1 : r;
+      this.minColumn = isNaN(c) ? 1 : c;
       this.makeFit();
     },
   },
@@ -201,11 +243,38 @@ export default {
 </script>
 
 <style>
+.drop-listener {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  height: 100svh;
+  background-color: red;
+  z-index: 1000;
+  visibility: hidden;
+}
+
+.drop-listener.active {
+  visibility: visible;
+}
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: Arial, Helvetica, sans-serif;
+}
+
+.App {
+  height: 100vh;
+  height: 100svh;
+  display: flex;
+  flex-direction: column;
+}
+
+.table-wrapper {
+  width: 100%;
+  overflow: auto;
+  flex: 1;
 }
 
 table {
@@ -215,7 +284,6 @@ table {
 
 td {
   border: 1px solid rgb(206, 206, 206);
-  padding: 2px;
 }
 
 td input {
@@ -230,6 +298,11 @@ td input {
 td input:read-only {
   cursor: default;
 }
+
+td input:read-only::selection {
+  color: black;
+}
+
 .col.col-head-selected {
   background-color: rgba(175, 169, 255, 0.514);
   border-left: 1px solid black;
